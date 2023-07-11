408f1b2fe328c40153245c2c4a686001f1409ea1
Dzikri Nursyaban <dzikrin022@gmail.com>
2023-07-11 18:02:32 +0700

    Set cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import { getRecommenderSellerHandler, getAllSellersHandler, getAllBuyersHandler } from './handler'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors({
+    origin: '*'
+}))
 
 app.get('/recommender-seller', getRecommenderSellerHandler)
 app.get('/sellers', getAllSellersHandler)
