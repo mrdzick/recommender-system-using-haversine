0906b4a760009bf7243b07ab3535b046f4d1a58d
Dzikri Nursyaban <dzikrin022@gmail.com>
2023-07-18 14:29:09 +0700

    Set to be sticky component

--- src/components/ListRecommendedSellers.tsx
+++ src/components/ListRecommendedSellers.tsx
@@ -9,23 +9,24 @@ type ListRecommendedSellersProps = {
 
 const ListRecommendedSellers = ({ recommendedSellers, isLoading }: ListRecommendedSellersProps) => {
   return (
-    <section>
+    <section className="w-[30vw] h-screen overflow-y-auto">
       <div className="pl-1">
-        <ul role="list" className="divide-y divide-gray-100">
+        <ul role="list" className="divide-y w-full max-auto px-2 divide-gray-100">
         {recommendedSellers && !isLoading && recommendedSellers.map((seller, idx) => (
-          <li key={seller.sellerId} className="flex justify-between gap-x-6 py-5">
-            <div className="flex gap-x-4">
-              <img className="h-11 w-11 flex-none rounded-full bg-gray-50" src={generateAvatar(idx + 1)} alt="" />
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{seller.companyName}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{seller.sellerId}</p>
+          <li key={seller.sellerId} className="flex flex-col gap-x-6 py-5">
+            <div className="flex gap-x-4 justify-between">
+              <div className="flex gap-5 items-center flex-auto w-[50%]">
+                <img className="h-11 w-11 flex-none rounded-full bg-gray-50" src={generateAvatar(idx + 1)} alt="" />
+                <div className="min-w-0 flex-auto">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{seller.companyName}</p>
+                </div>
               </div>
-            </div>
-            <div className="hidden sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{seller.city}</p>
-              <p className="mt-1 text-xs leading-5 text-gray-500">
-                {seller.distance}
-              </p>
+              <div className="flex-auto w-[50%]">
+                <p className="text-sm leading-6 text-gray-900 seller-city capitalize">{seller.city.toLowerCase()}</p>
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  {seller.distance.toFixed(2)} Km
+                </p>
+              </div>
             </div>
           </li>
         ))}
